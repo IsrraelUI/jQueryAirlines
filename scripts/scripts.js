386f195510e9552ef00c207ad661a9736d062d76
Isrrael Gutierrez <Isrrael.Gutierrez@neudesic.com>
2018-05-13 18:21:34 -0400

    resolving excercise

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,49 @@
-console.log('running')
+var resultset1;
+var resultset2;
+var setColumns = function (resultset, target){
+    for(result of resultset){
+        var template = '<div class="columns content"><div class="column">'+ result.airline +'</div><div class="column">'+ result.origin +'</div><div class="column">'+ result.destination +'</div></div>';
+        target.append(template);
+    }
+}
+$.ajax({
+    type: "FlightTracker",
+    url: "https://flighttracker.kobrekim.com/odata/odata.ashx",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+        // console.log(response)
+    },
+    error: function(){
+        resultset1 =  [{
+            "airline":"JetBlue",
+            "departure_date": "8/21/2017 @ 16:24",
+            "arrival_date":"8/21/2017 @ 19:24",
+            "origin": "JFK",
+            "destination":"LAX",
+            "flight_number":"JBU1323"
+        }];
+        setColumns(resultset1, $('#flightsGrid'))
+    }
+});
+
+$.ajax({
+    type: "FlightTracker",
+    url: "https://flighttracker.kobrekim.com/odata/odata.ashx",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+        // console.log(response)
+    },
+    error: function(){
+        resultset2 = [{
+            "airline":"Delta",
+            "departure_date": "9/1/2017 @ 22:15",
+            "arrival_date":"9/1/2017 @ 1:42",
+            "origin": "LAX",
+            "destination":"LGA",
+            "flight_number":"DAL2787"
+        }];
+        setColumns(resultset2, $('#flightsGrid'))
+    }
+});
